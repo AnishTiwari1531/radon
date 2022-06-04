acead68b95402b62fa1e88f1034cd427c396d7d2
AnishTiwari1531 <anishtiwari1531@gmail.com>
2022-06-05 02:22:58 +0530

    Anish

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,9 +3,22 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
+
+
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+
+//books GET Api
+router.get("/getBooks",UserController.getBooks)
+
+//book create Api
+router.post("/createbook",UserController.createEntryofBook)
+
+module.exports = router;
+
+
 
 router.post("/createUser", UserController.createUser  )
 
