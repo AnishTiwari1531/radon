49a8621eb776a9cfece1c8456ade18ae995ffaaf
AnishTiwari1531 <anishtiwari1531@gmail.com>
2022-06-15 17:16:12 +0530

    Authorisation & Authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,18 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
+//#1
 const createUser = async function (abcd, xyz) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
   let data = abcd.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
+  // console.log(abcd.newAtribute);
   xyz.send({ msg: savedData });
 };
 
+//#2
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
   let password = req.body.password;
@@ -32,32 +34,17 @@ const loginUser = async function (req, res) {
     {
       userId: user._id.toString(),
       batch: "thorium",
-      organisation: "FUnctionUp",
+      organisation: "FunctionUp",
     },
-    "functionup-thorium"
+    "functionup-radon"
   );
   res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
+  res.send({ status: true, token: token });
 };
 
+
+//#3
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-  
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-thorium");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
@@ -66,12 +53,8 @@ const getUserData = async function (req, res) {
   res.send({ status: true, data: userDetails });
 };
 
+//#4
 const updateUser = async function (req, res) {
-// Do the same steps here:
-// Check if the token is present
-// Check if the token present is a valid token
-// Return a different error message in both these cases
-
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
@@ -80,43 +63,44 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new: true});
+  res.send({ status: "User updated", data: updatedUser });
 };
 
-const postMessage = async function (req, res) {
-    let message = req.body.message
-    // Check if the token is present
-    // Check if the token present is a valid token
-    // Return a different error message in both these cases
-    let token = req.headers["x-auth-token"]
-    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
-    let decodedToken = jwt.verify(token, 'functionup-thorium')
-
-    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
-    
-    //userId for which the request is made. In this case message to be posted.
-    let userToBeModified = req.params.userId
-    //userId for the logged-in user
-    let userLoggedIn = decodedToken.userId
-
-    //userId comparision to check if the logged-in user is requesting for their own data
-    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
-
-    let user = await userModel.findById(req.params.userId)
-    if(!user) return res.send({status: false, msg: 'No such user exists'})
-    
-    let updatedPosts = user.posts
-    //add the message to user's posts
-    updatedPosts.push(message)
-    let updatedUser = await userModel.findOneAndUpdate({_id: user._id},{posts: updatedPosts}, {new: true})
-
-    //return the updated user document
-    return res.send({status: true, data: updatedUser})
+//#5
+const del = async function (req, res) {
+  let userId=req.params.userId;
+    let user =await userModel.findById(userId)
+    console.log(user)
+     if (!user) {
+      return res.send("No such user exists");
+    }
+  let data = await userModel.findOneAndUpdate({_id:userId},
+    {isDeleted : true} , 
+    { new: true})
+    res.send({ status: true, msg: data })
 }
+
+//#6
+const postMessage = async function (req, res) {
+  let message = req.body.message
+  let userToBeModified = req.params.userId
+  let user = await userModel.findById(userToBeModified)
+  if(!user) return res.send({status: false, msg: 'No such user exists'})
+  let updatedPosts = user.posts
+  //add the message to user's posts
+  updatedPosts.push(message)
+  let updatedUser = await userModel.findOneAndUpdate({_id: user._id},{posts: updatedPosts}, {new: true})
+
+  //return the updated user document
+  return res.send({status: true, data: updatedUser})
+}
+
+
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.del = del
 module.exports.postMessage = postMessage
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const cmMw = require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser)
+router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+// router.get("/users/:userId", cmMw.auth, cmMw.authenticate, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+// router.put("/users/:userId", cmMw.auth, cmMw.authenticate,  userController.updateUser)
 
+// router.delete("/users/:userId", cmMw.auth, cmMw.authenticate,  userController.del)
+
+// router.post("/users/:userId/posts", cmMw.auth, cmMw.authenticate,  userController.postMessage)
+
+// router.get("/users/:userId", cmMw.auth, cmMw.authenticate, cmMw.authorise, userController.getUserData)
+
+// router.put("/users/:userId", cmMw.auth, cmMw.authenticate, cmMw.authorise, userController.updateUser)
+
+// router.delete("/users/:userId", cmMw.auth, cmMw.authenticate, cmMw.authorise, userController.del)
+
+// router.post("/users/:userId/posts", cmMw.auth, cmMw.authenticate, cmMw.authorise, userController.postMessage)
+
+router.post("/users/:userId/posts", cmMw.auth, cmMw.auth2, userController.postMessage)
 module.exports = router;
