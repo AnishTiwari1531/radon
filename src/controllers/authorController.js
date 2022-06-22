c70386f2fd314fd3d63c8ca15b33e185184fbe9c
AnishTiwari1531 <anishtiwari1531@gmail.com>
2022-06-22 16:09:07 +0530

    Phase -1 Project Final Push

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,55 +1,64 @@
 const authorModel = require("../models/authorModel")
+const validator = require("validator")
 
 //------------------------regex---------------------------//
 
 
-let emailRegex= /^[A-Za-z]{1}[A-Za-z0-9]{1,100}[@]{1}[A-Za-z0-9]{2,15}[.]{1}[A-Za-z0-9]{2,10}$/
+let emailRegex = /^[A-Za-z]{1}[A-Za-z0-9]{1,100}[@]{1}[A-Za-z0-9]{2,15}[.]{1}[A-Za-z0-9]{2,10}$/
 
 let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 
 
 //---------------------------------------------------------------//
 
+//Q-1
+// Author APIs /authors
+// Create an author - atleast 5 authors
+// Create a author document from request body. Endpoint: BASE_URL/authors
+
+//...............................................................//
 
 
 const createAuthor = async function (req, res) {
-   try{
-    let data = req.body
-    let {fname,lname,title,password,email}=data
-    if(Object.keys(data).length===0){
-        return res.status(400).send({Status:false, message:"Please provide the data"})
-    }
-    if(!fname || fname==""){
-        return res.status(400).send({Status:false, message:"Please provide fname"})
-    }
-    if(!lname || lname==""){
-        return res.status(400).send({Status:false, message:"Please provide lname"})
-    }
-    if(!title || title==""){
-        return res.status(400).send({Status:false, message:"Please provide title"})
-    }
-    if(!emailRegex.test(email)){
-        return res.status(400).send({Status:false, message:"Please enter valid email"}) 
-    }
-    if(email){
-        let checkemail= await authorModel.findOne({email:email})
-     
-        if(checkemail){
-            return res.status(400).send({Status:false, message:"Please provide another email, this email has been used"})
+    try {
+        let data = req.body
+        let { fname, lname, title, password, email } = data
+        if (Object.keys(data).length === 0) {
+            return res.status(400).send({ Status: false, message: "Please provide the data 🛑" })
         }
+        if (!fname || fname == "") {
+            return res.status(400).send({ Status: false, message: "Please provide fname 🛑" })
+        }
+        if (!lname || lname == "") {
+            return res.status(400).send({ Status: false, message: "Please provide lname 🛑" })
+        }
+        if (!title || title == "") {
+            return res.status(400).send({ Status: false, message: "Please provide title 🛑" })
+        }
+        if (!emailRegex.test(email)) {
+            return res.status(400).send({ Status: false, message: "Please enter valid email 🛑" })
+        }
+        if (email) {
+            let checkemail = await authorModel.findOne({ email: email })
+
+            if (checkemail) {
+                return res.status(400).send({ Status: false, message: "Please provide another email, this email has been used 🛑" })
+            }
+        }
+        if (!passwordRegex.test(password)) {
+            return res.status(400).send({ Status: false, message: "Please provide valid password 🛑" })
+        }
+
+        let savedData = await authorModel.create(data)
+        return res.status(201).send({ msg: savedData })
     }
-    if(!passwordRegex.test(password)){
-        return res.status(400).send({Status:false, message:"Please provide valid password"})
+    catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Server Error ❌", error: err.message })
     }
-    
-    let savedData = await authorModel.create(data)
-    return res.status(201).send({ msg: savedData })
-   }
-   catch (err) {
-    console.log("This is the error :", err.message)
-    res.status(500).send({ msg: "Server Error", error: err.message })
-  }
 }
+
+
 module.exports.createAuthor = createAuthor
 
 
--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -6,6 +6,22 @@ const blogModel = require("../models/blogModel")
 
 //---------------------------------------------------------------//
 
+//Q-2
+
+// POST /blogs
+// Create a blog document from request body. Get authorId in request body only.
+
+// Make sure the authorId is a valid authorId by checking the author exist in the authors collection.
+
+// Return HTTP status 201 on a succesful blog creation. Also return the blog document. The response should be a JSON object like this
+
+// Create atleast 5 blogs for each author
+
+// Return HTTP status 400 for an invalid request with a response body like this
+
+
+//...............................................................//
+
 const createBlog = async function (req, res) {
     try {
         let data = req.body
@@ -15,15 +31,29 @@ const createBlog = async function (req, res) {
             res.status(201).send({ msg: savedData })
         }
         else
-            res.status(400).send({ msg: "authorId is not valid" })
+            res.status(400).send({ msg: "authorId is not valid ⚠️" })
     }
     catch {
-        res.status(500).send({ status: false, msg: "server Error" });
+        res.status(500).send({ status: false, msg: "server Error ❌" });
     }
 }
 
 
 //---------------------------------------------------------------//
+
+//Q-3
+
+// GET /blogs
+// Returns all blogs in the collection that aren't deleted and are published
+// Return the HTTP status 200 if any documents are found. The response structure should be like this
+// If no documents are found then return an HTTP status 404 with a response like this
+// Filter blogs list by applying filters. Query param can have any combination of below filters.
+// By author Id
+// By category
+// List of blogs that have a specific tag
+// List of blogs that have a specific subcategory example of a query url: blogs?filtername=filtervalue&f2=fv2
+
+//................................................................//
 
 const getBlogs = async function (req, res) {
     try {
@@ -31,9 +61,9 @@ const getBlogs = async function (req, res) {
         if (a.length > 0)
             res.status(200).send(a)
         else
-            res.status(404).send({ status: false, msg: "Data Not Found" })
+            res.status(404).send({ status: false, msg: "Data Not Found ⚠️" })
     } catch {
-        res.status(500).send({ status: false, msg: "server Error" });
+        res.status(500).send({ status: false, msg: "server Error ❌" });
     }
 }
 
@@ -61,27 +91,39 @@ const filterBlogs = async function (req, res) {
 
         let data = await blogModel.find(filterdata)
         if (data.length === 0) {
-            return res.status(404).send({ status: false, msg: "No Data Found" })
+            return res.status(404).send({ status: false, msg: "No Data Found ⚠️" })
         }
         else
             return res.status(200).send(data)
     }
     catch {
-        res.status(500).send({ status: false, msg: "server Error" });
+        res.status(500).send({ status: false, msg: "server Error ❌" });
     }
 }
 
 
 //---------------------------------------------------------------//
 
+//Q-4
+
+// PUT /blogs/:blogId
+// Updates a blog by changing the its title, body, adding tags, adding a subcategory. (Assuming tag and subcategory received in body is need to be added)
+// Updates a blog by changing its publish status i.e. adds publishedAt date and set published to true
+// Check if the blogId exists (must have isDeleted false). If it doesn't, return an HTTP status 404 with a response body like this
+// Return an HTTP status 200 if updated successfully with a body like this
+// Also make sure in the response you return the updated blog document.
+
+//...............................................................//
+
+
 const blogs = async function (req, res) {
     try {
         let data = req.body
         let blogId = req.params.blogId
-        if (!blogId) return res.status(404).send({ status: false, msg: "blogId is required" })
+        if (!blogId) return res.status(404).send({ status: false, msg: "blogId is required ⚠️" })
         let findblog = await blogModel.findById(blogId)
         if (!findblog) return res.status(404).send({ msg: "blogId  is invalid" })
-        if (findblog.isDeleted == true) return res.status(404).send({ msg: "Blog is already deleted " })
+        if (findblog.isDeleted == true) return res.status(404).send({ msg: "Blog is already deleted ⚠️" })
         if (findblog.isDeleted == false) {
             let updatedBlog = await blogModel.findOneAndUpdate({ _id: blogId }, {
                 $set: {
@@ -100,12 +142,20 @@ const blogs = async function (req, res) {
             return res.status(200).send(updatedBlog)
         }
     } catch {
-        res.status(500).send({ status: false, msg: "server Error" });
+        res.status(500).send({ status: false, msg: "server Error ❌" });
     }
 }
 
 
 //---------------------------------------------------------------//
+
+//Q-5
+
+// DELETE /blogs/:blogId
+// Check if the blogId exists( and is not deleted). If it does, mark it deleted and return an HTTP status 200 without any response body.
+// If the blog document doesn't exist then return an HTTP status of 404 with a body like this
+
+//...............................................................//
 
 const deleting = async function (req, res) {
     try {
@@ -114,27 +164,33 @@ const deleting = async function (req, res) {
         if (data) {
             if (data.isDeleted == false) {
                 data2 = await blogModel.findOneAndUpdate({ _id: id }, { isDeleted: true, deletedAt: new Date() }, { new: true })
-                res.status(200).send({ status: true, msg: "data deleted" })
+                res.status(200).send({ status: true, msg: "data deleted ⚠️" })
             } else {
-                res.status(200).send({ status: false, msg: "data already deleted" })
+                res.status(200).send({ status: false, msg: "data already deleted ⚠️" })
             }
         } else {
-            return res.status(404).send({ status: false, msg: "id does not exist" })
+            return res.status(404).send({ status: false, msg: "id does not exist ⚠️" })
         }
 
     }
     catch {
-        res.status(500).send({ status: false, msg: "server Error" });
+        res.status(500).send({ status: false, msg: "server Error ❌" });
     }
 }
 
 
 //---------------------------------------------------------------//
 
+//Q-6
+// DELETE /blogs?queryParams
+// Delete blog documents by category, authorid, tag name, subcategory name, unpublished
+// If the blog document doesn't exist then return an HTTP status of 404 with a body like this
+//...............................................................//
+
 const deleteSpecific = async function (req, res) {
     try {
         let query = req.query
-        let filterdata = { isDeleted: false, isPublished : false }
+        let filterdata = { isDeleted: false, isPublished: false }
         let { category, subcategory, tags, authorId } = query
         if (authorId) {
             filterdata.authorId = authorId
@@ -148,20 +204,20 @@ const deleteSpecific = async function (req, res) {
         if (tags) {
             filterdata.tags = tags
         }
-      
+
         let data = await blogModel.find(filterdata)
 
         if (data.length !== 0) {
-            filterdata.isDeleted=true
-            let updatedData = await blogModel.findOneAndUpdate({$or : {authorId:authorId}},{isDeleted:true},{new:true})
-            return res.status(200).send({ status: true, msg: "data is deleted" })
+            filterdata.isDeleted = true
+            let updatedData = await blogModel.findOneAndUpdate({ authorId: authorId }, { isDeleted: true }, { new: true })
+            return res.status(200).send({ status: true, msg: "data is deleted ⚠️" })
         }
         else {
-            return res.status(404).send({ status: false, msg: "No Data Found" })
+            return res.status(404).send({ status: false, msg: "No Data Found ⚠️" })
         }
     }
-    catch (err) {
-        res.status(500).send({ status: false, msg: err.message });
+    catch {
+        res.status(500).send({ status: false, msg: "Server Error ❌" });
     }
 }
 
@@ -169,6 +225,5 @@ const deleteSpecific = async function (req, res) {
 //---------------------------------------------------------------//
 
 module.exports = { createBlog, getBlogs, filterBlogs, blogs, deleting, deleteSpecific }
-
 
 //---------------------------------------------------------------//
